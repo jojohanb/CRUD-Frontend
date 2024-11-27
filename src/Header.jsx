ec95bd7485e720d78c25976de3b96f13d562ec82
jova <130059240+jojohanb@users.noreply.github.com>
2024-11-27 12:00:30 -0300

    ta indo

---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+
+function Header() {
+  return (
+      <div className="p-6 bg-[#0db1fd]">
+        <h1>agendasMG</h1>
+        <h3>Agendamentos</h3>
+        <h3>Dentistas</h3>
+        <div>
+        <h3>Murilo R R</h3>
+        <img className="rounded-full w-32 h-32 object-cover" src="https://i.pinimg.com/736x/76/f3/f3/76f3f3007969fd3b6db21c744e1ef289.jpg" alt="" />
+        </div>
+
+
+
+      
+      
+
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Header from './Header.jsx'
+// import Footer from './Footer.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Header />
+    {/* <Footer /> */}
+  </StrictMode>,
+)
