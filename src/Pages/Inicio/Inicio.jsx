23c21c820fc86262b9a3af51a0a91d9c23814ac0
jojohanb <130059240+jojohanb@users.noreply.github.com>
2024-12-12 20:06:22 -0300

    ok dnv

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -163,6 +163,10 @@ function Inicio() {
             </div>
           </form>
         </div>)}
+        
+      <footer className="bg-[#2e77ff] text-white p-5 fixed bottom-0 w-full text-center">
+        <p className="text-sm"> MaGendas - Faça seus Agendamentos conosco</p>
+      </footer>
     </div>
   );
 }
